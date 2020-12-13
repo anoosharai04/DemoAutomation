18f3d592cda29d2c69965209f238e2600008527c
Anoosha Rai <Anoosha.Rai@mnp.ca>
2020-12-13 11:27:25 -0700

    Add project files.

---
+++ LoginTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace WebDriver_CSharp_Example
+{
+
+
+    [TestFixture]
+    public class LoginTest
+    {
+        private IWebDriver driver;
+        public string homeURL;
+
+        [Test(Description = "Registration site for Automation")]
+        public void Login_is_on_home_page()
+        {
+            homeURL = "https://opensource-demo.orangehrmlive.com/";
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(homeURL);
+            WebDriverWait wait1 = new WebDriverWait(driver, System.TimeSpan.FromSeconds(15));
+            wait1.Until(driver => driver.FindElement(By.CssSelector("#divLogo")));
+            IWebElement element1 = driver.FindElement(By.XPath("//*[@id='logInPanelHeading']"));
+            Assert.AreEqual("LOGIN Panel", element1.GetAttribute("innerText"));
+            driver.FindElement(By.XPath("//*[@id='txtUsername']")).SendKeys("Admin");
+            driver.FindElement(By.CssSelector("#txtPassword")).SendKeys("admin123");
+            driver.FindElement(By.CssSelector("#btnLogin")).Click();
+            WebDriverWait wait2 = new WebDriverWait(driver, System.TimeSpan.FromSeconds(15));
+            wait2.Until(driver => driver.FindElement(By.CssSelector("#branding")));
+            driver.Close();
+
+        }
+    }
+
+}
---
+++ ReadME.md
@@ -0,0 +1,14 @@
+This solution is to demonstrate automated testing using C# and Selenium. 
+
+PRE-REQUISITES:
+To create and run WebDriver tests in C# using Visual Studio you’ll need the following components:
+
+- Visual Studio
+- Test framework
+- WebDriver’s C# bindings
+
+HOW TO PULL THE SOURCE PROJECT FROM GIT REPO-
+1. Open Visual Studio Enterprise/Community software.
+2. Use File => New => Project.
+3. From the Project templates, select C# class library (.NET Framework) option.
+4.
